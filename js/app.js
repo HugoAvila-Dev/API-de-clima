316587fa92993ac9eff5bb62ced2ebd8aa12523e
Hugo <hhugo.avila97@gmail.com>
2023-09-19 10:53:12 -0400

    selectores y funcion de buscar clima

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.addEventListener('load', () => {
+    formulario.addEventListener('submit', buscarClima)
+})
+
+
+function buscarClima(e) {
+    e.preventDefault();
+
+    console.log('Buscando el Clima...')
+}
