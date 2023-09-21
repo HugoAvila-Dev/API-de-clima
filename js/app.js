c83dd4cc14d013e7ee7c3e66694e3618292e9bf9
Hugo <hhugo.avila97@gmail.com>
2023-09-20 20:52:03 -0400

    feat: :rocket: Spinner agregado

--- js/app.js
+++ js/app.js
@@ -48,14 +48,15 @@ function mostrarError(mensaje) {
 function consultarAPI(ciudad, pais) {
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${API_KEY}`
-console.log(url)
-    fetch(url) 
+
+    Spinner(); //Muesta un Spinner de carga
+    fetch(url)
         .then(respuesta => respuesta.json())
         .then(datos => {
 
             limpiarHTML(); //Limpiar el HTML previo
-            
-            if(datos.cod === '404') {
+
+            if (datos.cod === '404') {
                 mostrarError('Ciudad no encotrada');
                 return;
             }
@@ -67,7 +68,7 @@ console.log(url)
 
 function mostrarClima(datos) {
 
-    const { name, main: { temp, temp_max, temp_min }} = datos;
+    const { name, main: { temp, temp_max, temp_min } } = datos;
 
     const centigrados = kelvinACentigrados(temp);
     const max = kelvinACentigrados(temp_max);
@@ -76,7 +77,7 @@ function mostrarClima(datos) {
     const nombreCiudad = document.createElement('p');
     nombreCiudad.textContent = `Clima en ${name}`;
     nombreCiudad.classList.add('font-bold', 'text-2xl');
-    
+
     const actual = document.createElement('p');
     actual.innerHTML = `${centigrados} &#8451`;
     actual.classList.add('font-bold', 'text-6xl');
@@ -87,7 +88,7 @@ function mostrarClima(datos) {
 
     const tempMinima = document.createElement('p');
     tempMinima.innerHTML = `Min: ${min} &#8451`
-    tempMinima.classList.add('text-xl')
+    tempMinima.classList.add('text-xl');
 
     const resultadoDiv = document.createElement('div');
     resultadoDiv.classList.add('text-center', 'text-white');
@@ -102,7 +103,31 @@ function mostrarClima(datos) {
 const kelvinACentigrados = grados => parseInt(grados - 273.15)
 
 function limpiarHTML() {
-    while(resultado.firstChild){
+    while (resultado.firstChild) {
         resultado.removeChild(resultado.firstChild)
     }
+}
+
+function Spinner() {
+
+    limpiarHTML();
+    
+    const divSpinner = document.createElement('div');
+    divSpinner.classList.add('sk-fading-circle');
+    divSpinner.innerHTML = `
+        <div class="sk-circle1 sk-circle"></div>
+        <div class="sk-circle2 sk-circle"></div>
+        <div class="sk-circle3 sk-circle"></div>
+        <div class="sk-circle4 sk-circle"></div>
+        <div class="sk-circle5 sk-circle"></div>
+        <div class="sk-circle6 sk-circle"></div>
+        <div class="sk-circle7 sk-circle"></div>
+        <div class="sk-circle8 sk-circle"></div>
+        <div class="sk-circle9 sk-circle"></div>
+        <div class="sk-circle10 sk-circle"></div>
+        <div class="sk-circle11 sk-circle"></div>
+        <div class="sk-circle12 sk-circle"></div>
+    `;
+    resultado.appendChild(divSpinner);
+
 }
